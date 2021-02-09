5a0806387da626fb0b42d7d7dffaa98b3d4ef1d0
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-02-09 19:58:34 +0900

    refactor: to components, header menu

---
+++ components/molecules/headers/header.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">
+          <img src="@/assets/image/wallet.png" width="33" height="28">
+        </nuxt-link>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasic"
+          @click="toggleMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasic" class="navbar-menu" :class="{ 'is-active': visibled }">
+        <div class="navbar-start">
+          <nuxt-link to="/" class="navbar-item">
+            Home
+          </nuxt-link>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <nuxt-link to="/credits" class="navbar-item">Credits</nuxt-link>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visibled: false
+    }
+  },
+
+  methods: {
+    toggleMenu () {
+      this.visibled = !this.visibled
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,15 @@
 <template>
   <div>
-    <header>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <nuxt-link to="/" class="navbar-item">
-            <img src="@/assets/image/wallet.png" width="33" height="28">
-          </nuxt-link>
-
-          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <nuxt-link to="/" class="navbar-item">
-              Home
-            </nuxt-link>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
-                More
-              </a>
-
-              <div class="navbar-dropdown">
-                <nuxt-link to="/credits" class="navbar-item">Credits</nuxt-link>
-              </div>
-            </div>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <a class="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a class="button is-light">
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <default-header />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import defaultHeader from '@/components/molecules/headers/header'
+export default {
+  components: {
+    defaultHeader
+  }
+}
+</script>
