cb670c1414977a7b2a01d04ac876cd756094f608
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-03-01 22:31:21 +0900

    feat: upload csv

--- pages/credits/index.vue
+++ pages/credits/index.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="credits_index">
     <section class="section">
-      <h1 class="title">クレジット情報</h1>
-      <h2 class="subtitle">
-        エポスカードの毎月のデータを閲覧できます
-      </h2>
+      <div class="columns">
+        <div class="column is-9">
+          <h1 class="title">クレジット情報</h1>
+          <h2 class="subtitle">
+            エポスカードの毎月のデータを閲覧できます
+          </h2>
+        </div>
+        <div class="column is-3 file_upload">
+          <div class="columns">
+            <div class="column">
+              <input type="date" v-model="form.billingDate">
+            </div>
+            <div class="column">
+              <label for="csv_file">
+                <a class="icon is-large fa-lg has-text-link" data-balloon="size: 3x">
+                  <font-awesome-icon :icon="['fas', 'folder-plus']" />
+                </a>
+                <input type="file" id="csv_file" style="display:none;" accept="text/csv" @change="upload($event)">
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
     <section class="">
       <div class="tabs">
@@ -69,7 +88,11 @@ export default {
         { showabled: false },
         { showabled: false },
         { showabled: false },
-      ]
+      ],
+      form: {
+        csv: '',
+        billingDate: '2021-02-01'
+      }
     }
   },
   methods: {
@@ -77,13 +100,37 @@ export default {
       return index === this.activeTabIndex
     },
     toActive (index) {
+      // TODO: query string化するのが良い気がする
       return this.activeTabIndex = index
     },
     switchShowableDetail (index) {
       return this.showables[index].showabled = !this.showables[index].showabled
     },
-    displayShowableDetail(index) {
+    displayShowableDetail (index) {
       return this.showables[index].showabled ? '▼' : '▶'
+    },
+    upload (event) {
+      if (event.target.files[0] === undefined) {
+        return
+      }
+      console.log(event.target.files[0])
+      this.form.csv = event.target.files[0]
+      const formData = new FormData
+      const [year, month] = this.form.billingDate.split('-')
+      formData.append('billing_month', `${year}${month}`)
+      formData.append('file', this.form.csv)
+      // TODO: base url 的なやつ
+      this.$axios.post('http://localhost/api/credits/import',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then(response => {
+          console.log(response.data);
+        }).catch(error => {
+          console.log(error);
+        })
     }
   }
 }
@@ -94,5 +141,9 @@ export default {
   .credit_tree {
     padding-left: 10px;
   }
+}
+.file_upload {
+  text-align: right;
+  vertical-align: middle;
 }
 </style>
