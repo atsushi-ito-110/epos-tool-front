2594a5a9d811b1eba64a4ab80a8d54ce200f48ed
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-03-13 16:02:52 +0900

    feat: csv import

--- pages/credits/index.vue
+++ pages/credits/index.vue
@@ -13,7 +13,13 @@
             <a class="icon is-large fa-lg has-text-link" data-balloon="size: 3x">
               <font-awesome-icon :icon="['fas', 'folder-plus']" />
             </a>
-            <input type="file" id="csv_file" style="display:none;" accept="text/csv" @change="upload($event)">
+            <input
+              type="file"
+              id="csv_file"
+              style="display:none;"
+              accept="text/csv"
+              @change="upload($event)"
+            />
           </label>
           {{ activedMonth }}
         </div>
@@ -23,17 +29,25 @@
       <div class="tabs">
         <ul>
           <li
-            v-for="(item, index) in yearMonths"
+            v-for="(credit, index) in credits"
             :key="index"
-            :class="{ 'is-active': activedTab(item.yearMonth) }"
+            :class="{ 'is-active': activedTab($dayjs(credit.billing_month).format('YYYY-MM')) }"
             :data-id="index"
           >
-            <nuxt-link :to="`/credits?year_month=${item.yearMonth}`" >{{ item.yearMonth }}</nuxt-link>
+            <nuxt-link :to="`/credits?year_month=${$dayjs(credit.billing_month).format('YYYY-MM')}`" >
+              {{ $dayjs(credit.billing_month).format('YYYY-MM') }}
+            </nuxt-link>
+          </li>
+          <li>
+            <font-awesome-icon :icon="['fas', 'plus']" />
           </li>
         </ul>
       </div>
       <div class="credit_tree">
-        <div v-if="activedMonth === '2021-02' && $fetchState.pending === false">
+        <!-- <pre>
+          {{ $fetchState.pending }}
+        </pre> -->
+        <div v-if="credit && $fetchState.pending === false">
           <p>金額: {{ totalAmount }}</p>
           <ul class="tree pl_10">
             <li v-for="(row, index) in credit.credit_details" :key="index">
@@ -54,10 +68,12 @@
 export default {
   data () {
     return {
+      // TODO: 動的化
       yearMonths: [
         { yearMonth: '2021-02' },
         { yearMonth: '2021-01' },
-        { yearMonth: '2020-12' }
+        { yearMonth: '2020-12' },
+        { yearMonth: '2020-11' },
       ],
       activedMonth: '2021-02',
       showables: [
@@ -71,16 +87,23 @@ export default {
       },
       credit: {
         credit_details: []
-      }
+      },
+      credits: []
     }
   },
 
   async fetch () {
+    console.log('fetch')
+    // console.log(this.credits, this.$dayjs().format('YYYY-MM'))
+    if (this.credits.length === 0) {
+      this.fetchCredit()
+    }
+    this.credit = null
     // TODO: fetchState.pendingで画面のくるくる実装したい
     if (this.$route.query.year_month) {
       this.activedMonth = this.$route.query.year_month
     }
-    await this.$axios.get('http://localhost/api/credits/' + this.activedMonth, {
+    await this.$axios.get('api/credits/' + this.activedMonth, {
     }).then((response) => {
       console.log(response)
       this.credit = response.data.credit
@@ -94,7 +117,7 @@ export default {
   //   this.activedMonth = newQuery.year_month
   //   console.log(this.activedMonth)
   //   this.$refetch()
-  //   // this.$axios.get('http://localhost/api/credits/show', {
+  //   // this.$axios.get('api/credits/show', {
   //   //   year_month: this.yearMonth
   //   // }).then((response) => {
   //   //   console.log(response)
@@ -133,31 +156,45 @@ export default {
       console.log(event.target.files[0])
       this.form.csv = event.target.files[0]
       const formData = new FormData
-      const [year, month] = this.form.billingDate.split('-')
+      const [year, month] = this.activedMonth.split('-')
       formData.append('billing_month', `${year}${month}`)
       formData.append('file', this.form.csv)
       // TODO: store化
       // TODO: base url 的なやつ
-      this.$axios.post('http://localhost/api/credits/import',
+      this.$axios.post('api/credits/import',
         formData,
         {
           headers: {
             'Content-Type': 'multipart/form-data'
           }
         }).then(response => {
-          console.log(response.data);
+          console.log(response.data)
+          this.$fetch()
         }).catch(error => {
           console.log(error);
+        })
+    },
+    fetchCredit () {
+      this.$axios.get('api/credits/')
+        .then((response) => {
+          console.log(response)
+          this.credits = response.data.credits
+        }).catch((error) => {
+          console.log(error)
         })
     }
   },
   created () {
+    console.log('created')
     if (this.$route.query.year_month === undefined) {
       this.activedMonth = this.yearMonths[0].yearMonth
     } else {
       this.activedMonth = this.$route.query.year_month
     }
     console.log(this.activedMonth)
+  },
+  mounted () {
+    console.log('mounted')
   }
 }
 </script>
